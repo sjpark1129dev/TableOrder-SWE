767c577a5d2588bc64c0c373cfb7c70f1c8ca02d
sjtemp62 <sjtemp62@gmail.com>
2025-05-10 04:22:23 +0900

    First-Commit & 테이블 관리 페이지 커밋

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module deu.swe2.tableorder {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens deu.swe2.tableorder to javafx.fxml;
+    exports deu.swe2.tableorder;
+}
